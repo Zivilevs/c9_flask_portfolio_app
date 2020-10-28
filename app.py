7cc91f63435866b1cbec09dbf2bb6fd1e0f6ef5c
Zivile V. Silveira <zivileval@yahoo.com>
2020-10-28 08:46:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	new_name = name + " likes bananas a lot!"
+	new_name = "\n" + name + " likes bananas a lot!"
 	return render_template('index.html', name=new_name)
 
 
